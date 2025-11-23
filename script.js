bdbeae83dc8e5354c10f42fac7c1db3343b43c32
RanaMuhammadAmaan <x24327085@student.ncirl.ie>
2025-11-23 10:53:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleText1() {
+		
+		let text = document.getElementById("para1");
+		text.style.display = "block";
+		
+}
+
+function toggleText2() {
+		
+		let text = document.getElementById("para2");
+		text.style.display = "block";
+		
+}
+
+function toggleText3() {
+		
+		let text = document.getElementById("para3");
+		text.style.display = "block";
+		
+}
